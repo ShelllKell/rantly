bea2d68b0228e3c751e48b0d0c6abf15ed732a70
Shelby Kelly <shelby.kelly@colorado.edu>
2014-10-21 13:09:19 -0600

    unfollow from following page works...see that bobby?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,13 @@ class User < ActiveRecord::Base
   has_many :follows, foreign_key: :followee_id
 
 
-  validates :username, presence: {error: "Username Required."}
+  validates :username, presence: {error: "Username required, must be unique."}
   validates :password, length: {minimum: 8}
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, length: {maximum: 500}
 
 
-  def following(user)
-    @follow = Follow.find_by(follower_id: current_user.id, followee_id: user.id)
 
-    unless current_user(@follow)
-      link_to "Follow", user_follows_path(user), method: :post
-    else
-      link_to "Unfollow", user_follow_path(user.id, @follow), method: :delete
-    end
-  end
 
 end
